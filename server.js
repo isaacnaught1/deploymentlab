8cf5deb3115f43f9aba5a9759eb63f47538ed513
Isaac Naughton <isaacnaughton01@gmail.com>
2023-03-22 16:17:53 -0600

    second commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+app.use(express.static(`${__dirname}/public`))
+
 app.listen(4000, () => {
     console.log('app is up on 4000')
 })
